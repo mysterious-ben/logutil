e0cee90a5b491ad034b0406ebf67abaa1d8a254f
Mysterious Ben <datascience@tuta.io>
2021-05-17 11:23:57 +0300

    chore: update test logging messages

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,18 +4,18 @@ from logutil import get_logging_logger, get_loguru_logger, init_logging, init_lo
 def test_basic_logging():
     init_logging()
     logger = get_logging_logger()
-    logger.debug("info message")
+    logger.debug("debug message")
     logger.info("info message")
-    logger.warning("info message")
-    logger.error("info message")
-    logger.critical("info message")
+    logger.warning("warning message")
+    logger.error("error message")
+    logger.critical("critical message")
 
 
 def test_basic_loguru():
     init_loguru()
     logger = get_loguru_logger()
-    logger.debug("info message")
+    logger.debug("debug message")
     logger.info("info message")
-    logger.warning("info message")
-    logger.error("info message")
-    logger.critical("info message")
+    logger.warning("warning message")
+    logger.error("error message")
+    logger.critical("critical message")
