f33fbe55138070a48afae2ef1564e918dd4ca90f
Mysterious Ben <datascience@tuta.io>
2024-04-24 11:22:14 +0200

    fix: pre-commit config

--- logutil/__init__.py
+++ logutil/__init__.py
@@ -1,2 +1,2 @@
-from logutil.src.init_logging import get_logging_logger, init_logging
-from logutil.src.init_loguru import get_loguru_logger, init_loguru
+from logutil.src.init_logging import get_logging_logger, init_logging  # noqa: F401
+from logutil.src.init_loguru import get_loguru_logger, init_loguru  # noqa: F401
